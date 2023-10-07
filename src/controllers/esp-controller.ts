df594c21b2bc94fb85061bf799d7e0970493275c
Pericles <periclesebmendes@gmail.com>
2023-10-07 13:24:12 -0300

    refactor: cleaned code

--- src/controllers/esp-controller.ts
+++ src/controllers/esp-controller.ts
@@ -4,11 +4,11 @@ import { esp } from "../utils/helpers";
 import httpStatus from "http-status";
 
 export async function getData(req: Request, res: Response) {
-    try {
-        const result = await axios.get(`${esp.url}/random-number`);
-        const resposta = String(result.data)
-        res.send(resposta);
-    } catch (error) {
-        return res.sendStatus(httpStatus.BAD_REQUEST);
-    }
+  try {
+    const result = await axios.get(`${esp.url}/random-number`);
+    const resposta = String(result.data);
+    res.send(resposta);
+  } catch (error) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
 }
